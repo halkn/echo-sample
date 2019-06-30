f8c7578365050abcd474dc14e9ad906608b54b69
halkn <h.mhty0wia.s@gmail.com>
2019-06-30 16:49:40 +0900

    Fix : golint's indications

--- registory/registory.go
+++ registory/registory.go
@@ -9,22 +9,27 @@ import (
 	"github.com/halkn/echo-sample/usecase"
 )
 
+// InjectDB is Dependency injector for *sql.DB
 func InjectDB() *sql.DB {
 	return datastore.NewDB()
 }
 
-func InjectSqlHandler() *datastore.SQLHandler {
+// InjectSQLHandler is Dependency injector for SQLHandler
+func InjectSQLHandler() *datastore.SQLHandler {
 	return datastore.NewSQLHandler(InjectDB())
 }
 
+// InjectTodoRepository is Dependency injector for TodoRepository
 func InjectTodoRepository() *gateway.TodoRepository {
-	return gateway.NewTodoRepository(InjectSqlHandler())
+	return gateway.NewTodoRepository(InjectSQLHandler())
 }
 
+// InjectTodoInteractor is Dependency injector for TodoInteractor
 func InjectTodoInteractor() *usecase.TodoInteractor {
 	return usecase.NewTodoInteractor(InjectTodoRepository())
 }
 
+// InjectTodoController is Dependency injector for TodoController
 func InjectTodoController() *controllers.TodoController {
 	return controllers.NewTodoController(InjectTodoInteractor())
 }
